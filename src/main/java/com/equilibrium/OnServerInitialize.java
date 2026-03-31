ef2959b6519257211862747b9e006ca85cbaa134
Lithewings <2631810335@qq.com>
2026-03-31 15:45:47 +0800

    Beta1.1.0

--- src/main/java/com/equilibrium/OnServerInitialize.java
+++ src/main/java/com/equilibrium/OnServerInitialize.java
@@ -142,7 +142,7 @@ public class OnServerInitialize implements ModInitializer {
 
             @Override
             public String getName() {
-                return "MITE:Equilibrium Beta v1.0.9";
+                return "MITE:Equilibrium Beta v1.1.0";
             }
 
             @Override
