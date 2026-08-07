e2f56ddc2ca90118df66b10f62f9f2b187cd6b2f
Lithewings <128245561+Lithewings@users.noreply.github.com>
2026-08-07 19:57:58 +0800

    Alpha 1.7(bugs fix)

--- src/main/java/com/equilibrium/mixin/player/PlayerManagerMixin.java
+++ src/main/java/com/equilibrium/mixin/player/PlayerManagerMixin.java
@@ -29,6 +29,7 @@ import static com.equilibrium.difficulty_entry.DifficultyEntryGetter.getGameBool
 import static com.equilibrium.difficulty_entry.DifficultyEntryRegister.ENABLE_MORE_SL_DAMAGE;
 import static com.equilibrium.difficulty_entry.DifficultyEntryRegister.ENABLE_NO_ANIMALS;
 import static com.equilibrium.difficulty_entry.DifficultyEntryUtil.onPlayerConnectSynchronizingGameRulesForBoolean;
+import static com.equilibrium.util.CommandExecutor.executeCommandWithSlash;
 
 
 @Mixin(PlayerList.class)
@@ -51,7 +52,7 @@ public abstract class PlayerManagerMixin {
                 leather.setCount(16);
                 player.getInventory().placeItemBackInInventory(leather);
             }
-
+            executeCommandWithSlash(player.server, "/give @p patchouli:guide_book[patchouli:book=\"miteequilibrium:equilibrium_guide\"]");
 
         }
         player.addEffect(new MobEffectInstance(MobEffects.BLINDNESS, 100, 255, false, false, false));
