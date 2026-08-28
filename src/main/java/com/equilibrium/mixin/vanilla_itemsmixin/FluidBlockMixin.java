4e4200fc4e04f58cb4e3d1bdc7299c06e77c32d9
Lithewings <128245561+Lithewings@users.noreply.github.com>
2026-08-28 12:58:00 +0800

    Alpha 1.8(sync:Fabric->NeoForge)

--- src/main/java/com/equilibrium/mixin/vanilla_itemsmixin/FluidBlockMixin.java
+++ src/main/java/com/equilibrium/mixin/vanilla_itemsmixin/FluidBlockMixin.java
@@ -1,6 +1,5 @@
 package com.equilibrium.mixin.vanilla_itemsmixin;
 
-import com.equilibrium.mixin.player.ClientPlayerEntityMixin;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerPlayer;
@@ -19,13 +18,13 @@ import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.material.FlowingFluid;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
-
-import java.util.Random;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LiquidBlock.class)
 public abstract class FluidBlockMixin extends Block implements BucketPickup {
@@ -58,8 +57,9 @@ public abstract class FluidBlockMixin extends Block implements BucketPickup {
 
 
 
-    @Override
-    public ItemStack pickupBlock(@Nullable Player player, LevelAccessor world, BlockPos pos, BlockState state) {
+    @Inject(method = "pickupBlock",at = @At("HEAD"),cancellable = true)
+    public void pickupBlock(Player player, LevelAccessor world, BlockPos pos, BlockState state, CallbackInfoReturnable<ItemStack> cir) {
+        cir.cancel();
         //玩家捞
         if(player!=null){
 
@@ -69,14 +69,15 @@ public abstract class FluidBlockMixin extends Block implements BucketPickup {
                     addParticle((Level)world,pos);
                 }
 
-                if (failToGetLava(player, world, pos)) return ItemStack.EMPTY;
+                if (failToGetLava(player, world, pos))
+                    cir.setReturnValue(ItemStack.EMPTY);
 
                 //没烧坏
                 if (player instanceof ServerPlayer)
                     //客户端渲染会有几个帧拿到岩浆桶又因为服务端计算应该-1而又消失的动画
-                    return new ItemStack(this.fluid.getBucket());
+                    cir.setReturnValue(new ItemStack(this.fluid.getBucket()));
                 else
-                    return ItemStack.EMPTY;
+                    cir.setReturnValue(ItemStack.EMPTY);
             }
             //捞源头,是岩浆,玩家下蹲
             else if ((Integer)state.getValue(LEVEL) == 0 && state.is(Blocks.LAVA) && player.isShiftKeyDown()){
@@ -84,45 +85,46 @@ public abstract class FluidBlockMixin extends Block implements BucketPickup {
                     addParticle((Level)world,pos);
                 }
 
-                if (failToGetLava(player, world, pos)) return ItemStack.EMPTY;
+                if (failToGetLava(player, world, pos))
+                    cir.setReturnValue(ItemStack.EMPTY);
 
                 //没烧坏
                 if (player instanceof ServerPlayer) {
                     //客户端渲染会有几个帧拿到岩浆桶又因为服务端计算应该-1而又消失的动画
                     world.setBlock(pos, Blocks.AIR.defaultBlockState(), Block.UPDATE_ALL_IMMEDIATE);
-                    return new ItemStack(this.fluid.getBucket());
+                    cir.setReturnValue(new ItemStack(this.fluid.getBucket()));
                 }
                 else
-                    return ItemStack.EMPTY;
+                    cir.setReturnValue(ItemStack.EMPTY);
             }
             //捞源头,不是岩浆,玩家下蹲
             else if ((Integer)state.getValue(LEVEL) == 0 && !(state.is(Blocks.LAVA)) && player.isShiftKeyDown()){
                 world.setBlock(pos, Blocks.AIR.defaultBlockState(), Block.UPDATE_ALL_IMMEDIATE);
                 if (player instanceof ServerPlayer)
                     //客户端渲染会有几个帧拿到岩浆桶又因为服务端计算应该-1而又消失的动画
-                    return new ItemStack(this.fluid.getBucket());
+                    cir.setReturnValue(new ItemStack(this.fluid.getBucket()));
                 else
-                    return ItemStack.EMPTY;
+                    cir.setReturnValue(ItemStack.EMPTY);
             }
 
             //捞源头,但不捞走,不是岩浆,玩家没有下蹲
             else if ((Integer)state.getValue(LEVEL) == 0 && !(state.is(Blocks.LAVA)) && !player.isShiftKeyDown()){
                 if (player instanceof ServerPlayer)
                     //客户端渲染会有几个帧拿到岩浆桶又因为服务端计算应该-1而又消失的动画
-                    return new ItemStack(this.fluid.getBucket());
+                    cir.setReturnValue(new ItemStack(this.fluid.getBucket()));
                 else
-                    return ItemStack.EMPTY;
+                    cir.setReturnValue(ItemStack.EMPTY);
             }
             else
-                return ItemStack.EMPTY;
+                cir.setReturnValue(ItemStack.EMPTY);
         }
 
 
         //海绵吸水等
         else if ( (Integer)state.getValue(LEVEL) == 0) {
-            return new ItemStack(this.fluid.getBucket());
+            cir.setReturnValue(new ItemStack(this.fluid.getBucket()));
         } else {
-            return ItemStack.EMPTY;
+            cir.setReturnValue(ItemStack.EMPTY);
         }
     }
 
